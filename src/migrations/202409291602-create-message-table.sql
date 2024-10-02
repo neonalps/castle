834b3a48a029f4dbdc3e4f712e4140436aa6aa18
Michael Stifter <michael@neonalps.at>
2024-10-02 07:32:19 +0200

    chore: make table names singular

--- src/migrations/202409291602-create-message-table.sql
+++ src/migrations/202409291602-create-message-table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE messages (
+CREATE TABLE message (
     timestamp TEXT,
     profile_id TEXT NOT NULL,
     content TEXT NOT NULL,
     is_encrypted BOOLEAN NOT NULL,
     PRIMARY KEY (timestamp, profile_id),
-    CONSTRAINT fk_profiles_messages FOREIGN KEY (profile_id) REFERENCES profiles (id)
+    CONSTRAINT fk_message_profile FOREIGN KEY (profile_id) REFERENCES profile(id)
 );
--- src/migrations/202409291605-create-messages-merkle-table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE messages_merkle (
-    profile_id TEXT,
-    merkle TEXT,
-    PRIMARY KEY (profile_id),
-    CONSTRAINT fk_profiles_messages_merkle FOREIGN KEY (profile_id) REFERENCES profiles (id)
-);
